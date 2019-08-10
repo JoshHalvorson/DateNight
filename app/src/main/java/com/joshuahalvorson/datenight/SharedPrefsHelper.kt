b926948a2669cf7537e8460517b230ea1ef7b1ff
JoshHalvorson <joshhalvorson16@gmail.com>
2019-08-10 13:36:41 -0700

    Add shared prefs helper

---
+++ app/src/main/java/com/joshuahalvorson/datenight/SharedPrefsHelper.kt
@@ -0,0 +1,31 @@
+package com.joshuahalvorson.datenight
+
+import android.content.SharedPreferences
+
+class SharedPrefsHelper(private val sharedPrefs: SharedPreferences?) {
+    companion object {
+        const val PREFERENCE_FILE_KEY = "favorite_restaurants"
+        const val RESTAURANT_ID_KEY = "restaurant_id"
+        const val RESTAURANT_NAME_KEY = "restaurant_name"
+    }
+
+    fun put(key: String, value: String?) {
+        sharedPrefs?.edit()?.putString(key, value)?.apply()
+    }
+
+    fun put(key: String, value: Int) {
+        sharedPrefs?.edit()?.putInt(key, value)?.apply()
+    }
+
+    fun get(key: String, defaultValue: String?): String? {
+        return sharedPrefs?.getString(key, defaultValue)
+    }
+
+    fun get(key: String, defaultValue: Int): Int? {
+        return sharedPrefs?.getInt(key, defaultValue)
+    }
+
+    fun remove(key: String) {
+        sharedPrefs?.edit()?.remove(key)?.apply()
+    }
+}
