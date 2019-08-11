88c1849979f405deab77d27fef8a7dfdde58346e
JoshHalvorson <joshhalvorson16@gmail.com>
2019-08-11 13:36:43 -0700

    Add functionality for clicking on saved restaurant and getting details

--- app/src/main/java/com/joshuahalvorson/datenight/adapter/SavedRestaurantsListAdapter.kt
+++ app/src/main/java/com/joshuahalvorson/datenight/adapter/SavedRestaurantsListAdapter.kt
@@ -7,13 +7,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.joshuahalvorson.datenight.R
-import com.joshuahalvorson.datenight.loadImageWithPicasso
 import com.joshuahalvorson.datenight.loadRatingImageWithPicasso
 import com.joshuahalvorson.datenight.model.SavedRestaurant
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.saved_restaurant_list_item.view.*
 
-class SavedRestaurantsListAdapter(private val restaurants: Array<SavedRestaurant>) : RecyclerView.Adapter<SavedRestaurantsListAdapter.ViewHolder>() {
+class SavedRestaurantsListAdapter(
+    private val restaurants: Array<SavedRestaurant>,
+    private val callback: OnListItemClick
+) : RecyclerView.Adapter<SavedRestaurantsListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): ViewHolder {
         return ViewHolder(
@@ -23,7 +25,7 @@ class SavedRestaurantsListAdapter(private val restaurants: Array<SavedRestaurant
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.bindModel(restaurants[position])
+        viewHolder.bindModel(restaurants[position], callback)
     }
 
     override fun getItemCount() = restaurants.size
@@ -34,13 +36,20 @@ class SavedRestaurantsListAdapter(private val restaurants: Array<SavedRestaurant
         private val restaurantRating: ImageView = itemView.saved_restaurant_rating
         private val restaurantImage: ImageView = itemView.saved_restaurant_image
 
-        fun bindModel(restaurant: SavedRestaurant) {
+        fun bindModel(restaurant: SavedRestaurant, callback: OnListItemClick) {
+            itemView.setOnClickListener { callback.onListItemClick(restaurant) }
             restaurantName.text = restaurant.name
             restaurantPrice.text = restaurant.price
             restaurant.rating?.let { restaurantRating.loadRatingImageWithPicasso(it) }
             Picasso.get()
                 .load(restaurant.image_url)
+                .resize(500, 500)
+                .centerCrop()
                 .into(restaurantImage)
         }
+    }
+
+    interface OnListItemClick {
+        fun onListItemClick(restaurant: SavedRestaurant?)
     }
 }
--- app/src/main/java/com/joshuahalvorson/datenight/view/fragment/SavedRestaurantsFragment.kt
+++ app/src/main/java/com/joshuahalvorson/datenight/view/fragment/SavedRestaurantsFragment.kt
@@ -1,6 +1,5 @@
 package com.joshuahalvorson.datenight.view.fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,25 +10,42 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
+import com.daimajia.androidanimations.library.Techniques
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.joshuahalvorson.datenight.App
 import com.joshuahalvorson.datenight.R
-import com.joshuahalvorson.datenight.SharedPrefsHelper
+import com.joshuahalvorson.datenight.adapter.RestaurantReviewsListAdapter
 import com.joshuahalvorson.datenight.adapter.SavedRestaurantsListAdapter
+import com.joshuahalvorson.datenight.animateViewWithYoYo
 import com.joshuahalvorson.datenight.database.RestaurantDatabase
+import com.joshuahalvorson.datenight.model.Businesses
 import com.joshuahalvorson.datenight.model.SavedRestaurant
-import com.joshuahalvorson.datenight.toSavedRestaurant
+import com.joshuahalvorson.datenight.openUrlOnClick
 import com.joshuahalvorson.datenight.viewmodel.YelpViewModel
 import com.joshuahalvorson.datenight.viewmodel.YelpViewModelFactory
-import kotlinx.android.synthetic.main.fragment_saved_restaurants.*
+import kotlinx.android.synthetic.main.content_random_restaurant.*
+import kotlinx.android.synthetic.main.content_saved_restaurants.*
+import kotlinx.android.synthetic.main.restaurant_details_bottom_sheet.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SavedRestaurantsFragment : Fragment() {
+class SavedRestaurantsFragment : Fragment(), OnMapReadyCallback {
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isScrollGesturesEnabled = false
+    }
+
     @Inject
     lateinit var yelpViewModelFactory: YelpViewModelFactory
     private lateinit var yelpViewModel: YelpViewModel
+    private lateinit var mMap: GoogleMap
 
     private var db: RestaurantDatabase? = null
 
@@ -47,6 +63,11 @@ class SavedRestaurantsFragment : Fragment() {
                 RestaurantDatabase::class.java, getString(R.string.database_playlist_name)).build()
 
         }
+
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.restaurant_map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
         displayRestaurants()
     }
 
@@ -54,9 +75,16 @@ class SavedRestaurantsFragment : Fragment() {
         saved_restaurants_list.apply {
             layoutManager = LinearLayoutManager(context)
             GlobalScope.launch(Dispatchers.IO) {
-                adapter = db?.savedRestaurantsDao()?.getAllPlaylists()?.let { restaurants ->
+                adapter = db?.savedRestaurantsDao()?.getAllRestaurants()?.let { restaurants ->
                     SavedRestaurantsListAdapter(
-                        restaurants
+                        restaurants,
+                        object: SavedRestaurantsListAdapter.OnListItemClick {
+                            override fun onListItemClick(restaurant: SavedRestaurant?) {
+                                restaurant?.id?.let { id -> getRestaurant(id) }
+                                bottom_sheet_restaurant_details.visibility = View.GONE
+                            }
+
+                        }
                     )
                 }
             }
@@ -67,7 +95,53 @@ class SavedRestaurantsFragment : Fragment() {
         yelpViewModel.getRestaurant(id).observe(this, Observer {
             it?.let { restaurant ->
                 Log.i("businessResponse", restaurant.name)
+                setBottomSheetContent(restaurant)
             }
         })
+    }
+
+    private fun setBottomSheetContent(businesses: Businesses) {
+        //TODO("Fix crash when pressing multiple times on item, has to do with the network call for reviews")
+        //TODO("Fix layout, bottom sheet hides last item")
+        bottom_sheet_restaurant_details.visibility = View.VISIBLE
+        bottom_sheet_restaurant_details.animateViewWithYoYo(Techniques.SlideInUp, 500, 0)
+        mMap.clear()
+        bottom_sheet_restaurant_title.text = businesses.name
+        bottom_sheet_restaurant_categories.text = ""
+        var categories = 0
+        businesses.categories?.size?.let {
+            categories = it
+        }
+        for (i in 0 until categories) {
+            bottom_sheet_restaurant_categories.text =
+                "${bottom_sheet_restaurant_categories.text}${businesses.categories?.get(i)?.title} \n"
+        }
+
+        businesses.url?.let { url ->
+            context?.let { context ->
+                open_restaurant_on_yelp_imageview.openUrlOnClick(url, context)
+            }
+        }
+
+        businesses.coordinates?.latitude?.let { latitude ->
+            businesses.coordinates.longitude?.let { longitude ->
+                val location = LatLng(latitude, longitude)
+                mMap.addMarker(MarkerOptions().position(location).title(businesses.name))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+            }
+        }
+
+        businesses.alias?.let { alias ->
+            yelpViewModel.getRestaurantReviews(alias).observe(this, Observer { response ->
+                Log.i("reviewResponse", "${response.reviews?.size}")
+                bottom_sheet_restaurant_reviews.apply {
+                    layoutManager = LinearLayoutManager(context)
+                    response.reviews?.let { reviews ->
+                        adapter = RestaurantReviewsListAdapter(reviews)
+                    }
+                }
+            })
+        }
+        save_for_later_button.visibility = View.GONE
     }
 }
